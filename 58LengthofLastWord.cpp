b2cbed560828d02942116ad59bee227d56ab5565
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2023-04-12 20:24:28 -0700

    Create 58LengthofLastWord.cpp

---
+++ 58LengthofLastWord.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        istringstream str(s);
+        string temp;
+        while(str >> temp)
+        {
+
+        }
+        return temp.length();
+    }
+};
